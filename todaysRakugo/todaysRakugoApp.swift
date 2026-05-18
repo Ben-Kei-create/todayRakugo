23b85d83e77650d00104af5908c7620c0be3ee67
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-05-18 22:38:48 +0900

    Initial Commit

---
+++ todaysRakugo/todaysRakugoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todaysRakugoApp.swift
+//  todaysRakugo
+//
+//  Created by 茂木史明 on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct todaysRakugoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
